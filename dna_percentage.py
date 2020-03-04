92876acfb3679b8a9d4a165a2e5dc401ab0b3e14
Phuc Phan <phucphan@arsc-7047088.ddns.uark.edu>
2020-03-04 10:07:10 -0600

    first commit

---
+++ dna_percentage.py
@@ -0,0 +1,19 @@
+#!/usr/env python3
+input = "dna.txt"
+inputfile = open(input, "r")
+dna_sequence = inputfile.read()
+print(dna_sequence)
+a_count = dna_sequence.count("a")
+g_count = dna_sequence.count("g")
+t_count = dna_seuqence.count("t")
+c_count = dna_sequence.count("c")
+total_length = len(dna_sequence)
+aper = a_count/total_length*100
+tper = t_count/total_length*100
+cper = c_count/total_length*100
+gper = g_count/total_legnth*100
+print("Percent A : " + str(aper) + "%")
+print("Percent T : " + str(tper) + "%")
+print("Percent G : " + str(gper) + "%")
+print("Percent C : " + str(cper) + "%")
+inputfile.close()
