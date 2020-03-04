b806ae81f99e7d9543dad447b8055f52d9ee38cb
pphan00 <phucphan@uark.edu>
2020-03-04 11:07:54 -0600

    add round up to 2 decimal

--- dna_percentage.py
+++ dna_percentage.py
@@ -8,10 +8,10 @@ g_count = dna_sequence.count("g")
 t_count = dna_sequence.count("t")
 c_count = dna_sequence.count("c")
 total_length = len(dna_sequence)
-aper = a_count/total_length*100
-tper = t_count/total_length*100
-cper = c_count/total_length*100
-gper = g_count/total_length*100
+aper = round(a_count/total_length*100, 2)
+tper = round(t_count/total_length*100, 2)
+cper = round(c_count/total_length*100, 2)
+gper = round(g_count/total_length*100, 2)
 print("Percent A : " + str(aper) + "%")
 print("Percent T : " + str(tper) + "%")
 print("Percent G : " + str(gper) + "%")
