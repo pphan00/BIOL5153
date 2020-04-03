6ee9b386ac70202528c71ed9334ccf821bc11453
Phuc Phan <phucphan@uark.edu>
2020-04-03 11:53:12 -0500

    fixed

--- parseGFF.py
+++ parseGFF.py
@@ -1,17 +1,45 @@
 #! /usr/bin/env python3
 
-my_file = "watermelon.gff"
-open_file = open(my_file)
-read_file = open_file.read().rstrip('\n')
+input = 'watermelon.gff'
 
-if "similar" != read_file:
-	split_word = read_file.split()
-	remove_word = ['atpB-rbcL','ndhC-trnV','trnG-trfM','trnfM-rps14','trnL-trnL','trnM','psbM-trnD','trnT-psbD', 'rps12-trnV','petG-trnW','trnW','trnP', 'psbC', 'psbD','trnD', 'trnC', 'rpoB-trnC', 'rpoB', 'rpl32-trnL','trnH-psbA','tRNH','trnI(TAT)','with','rRNA','mRNA', 'repeat','codon,','codon"','region', 'terminus', 'predicted', 'editing', 'gene,','loop','repeat_region','codon', 'start', 'stop', 'subunit', 'possibly', 'of', 'not', 'gene', 'for', 'IR', 'ID', 'PubMed', 'C', 'DNA,', 'editting', 'gene', 'generation', 'inverted','(SSC)','RNA','type', 'within', 'tRNA', 'chloroplast-like', 'to', 'spacer', 'similar', 'see', 'misc_feature', 'intron', 'exon', 'larger', 'intact', 'green', 'grey47', 'functional', 'from', 'fragment', 'expanded', 'editting', 'embedded', 'creates', 'determined', 'codon', 'cis-spliced','GenBank', 'chloroplast', ';','Gene', 'Citrullus', 'lanatus', 'COLOR', 'blue', 'red', 'black', 'CDS', '+', '-', '.', 'LSC', 'Note', 'DNA', 'U', 'anticodon', '(LSC)', '/']
-	remove_total = remove_word
-	split_word.sort()
-	x = [u for u in split_word if u not in remove_total]
-	del x[0:488]
-	print(x)
+open_file = open(input, 'r')
 
+gene_list = []
+
+for line in open_file:
+
+	line = line.rstrip('\n')
+	
+	name_split = line.split('\t')
+
+	gene_name = name_split[8].split(';')
+
+	split_again = gene_name[0].split('Gene')
+
+	name_only = split_again[1]
+	
+	gene_list.append(name_only)
+
+	order_list = sorted(gene_list)
+	
+	print(order_list)
 
 open_file.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
