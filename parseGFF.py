7a4bf06ba4f5cadb582d70b2f29972a4bd505977
Phuc Phan <phucphan@uark.edu>
2020-04-06 09:23:52 -0500

    assignment 6 added

--- parseGFF.py
+++ parseGFF.py
@@ -1,30 +1,49 @@
 #! /usr/bin/env python3
 
-input = 'watermelon.gff'
+import csv
+import argparse
 
-open_file = open(input, 'r')
+#assignment 6
+parser = argparse.ArgumentParser(description = "This script parse a gff file and some other things")
+
+parser.add_argument("gff_input", help = "name of the GFF file")
+parser.add_argument("fasta_input", help = "name of the fasta file")
+
+args = parser.parse_args()
 
 gene_list = []
 
-for line in open_file:
-
-	line = line.rstrip('\n')
+with open(args.gff_input, 'r') as gff:
 	
-	name_split = line.split('\t')
-
-	gene_name = name_split[8].split(';')
-
-	split_again = gene_name[0].split('Gene')
-
-	name_only = split_again[1]
+	reader=csv.reader(gff, delimiter='\t')
 	
-	gene_list.append(name_only)
+	for line in reader:
+		if not line:
+			continue
+		else:
+			start = line[3]
+			end = line[4]
+			print(start, end)
+#assignment 5
+#for line in open_file:
 
-	order_list = sorted(gene_list)
+	#line = line.rstrip('\n')
 	
-	print(order_list)
+	#name_split = line.split('\t')
 
-open_file.close()
+	#gene_name = name_split[8].split(';')
+
+	#split_again = gene_name[0].split('Gene')
+
+	#name_only = split_again[1]
+	
+	#gene_list.append(name_only)
+
+	#order_list = sorted(gene_list)
+	
+	#print(order_list)
+
+gff.close()
 
 
 
