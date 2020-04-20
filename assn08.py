9c914b67dd901cb187c64adaea6d12df4cf0ec64
Phuc Phan <phucphan@uark.edu>
2020-04-20 01:07:17 -0500

    fix finditer, can now print all 8 occurences, first char, last char, length of occurence, position of first and last char, CANNOT print to a table

--- assn08.py
+++ assn08.py
@@ -4,14 +4,23 @@ import re
 
 concert = "Katherine went to the concert to see Catheryn and the Cathryn's. She ran into her friend Kathryn, who introduced Katherine to her friend Catherine. Toghether, they enjoyed the concert while texting inaudible snippets to their mutual friend, Kathrin. Their mercurial friend, katharine, felt left out."
 
-match = re.findall("[k|K|C]ath[a|e]?r[y|i]ne?", concert)
+pattern = "[k|K|C]ath[a|e]?r[y|i]ne?"
 
-if match:
-	print(match)
-	split = re.split("\s", match)
-	print(split[0])
-else:
-	print("No match")
+match = re.finditer(pattern, concert)
+
+for y in match:
+	result = y.group(0)
+	print(result)
+	#for x in y:
+		#print(x)
+	print("Length of match: " + str(len(result)))
+	print("First character: " + str(result[0]))
+	print("Last character: "+ str(result[-1]))
+	match_start = y.start()
+	match_end = y.end()
+	print("Position of first character is: " + str(match_start))
+	print("Position of last character is: " + str(match_end))
+
 
 
 
