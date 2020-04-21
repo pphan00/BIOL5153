56a64449f9566061db87b0800f8806291637712c
Phuc Phan <phucphan@uark.edu>
2020-04-21 18:13:16 -0500

    fix input as a table

--- assn08.py
+++ assn08.py
@@ -4,23 +4,20 @@ import re
 
 concert = "Katherine went to the concert to see Catheryn and the Cathryn's. She ran into her friend Kathryn, who introduced Katherine to her friend Catherine. Toghether, they enjoyed the concert while texting inaudible snippets to their mutual friend, Kathrin. Their mercurial friend, katharine, felt left out."
 
-pattern = "[k|K|C]ath[a|e]?r[y|i]ne?"
+pattern = re.compile("(k|c)ath[eaiu]*r(y|i)n{1,}e*", re.I)
 
 match = re.finditer(pattern, concert)
 
 for y in match:
 	result = y.group(0)
-	print(result)
-	#for x in y:
-		#print(x)
-	print("Length of match: " + str(len(result)))
-	print("First character: " + str(result[0]))
-	print("Last character: "+ str(result[-1]))
+	#print("Length of match: " + str(len(result)))
+	#print("First character: " + str(result[0]))
+	#print("Last character: "+ str(result[-1]))
 	match_start = y.start()
 	match_end = y.end()
-	print("Position of first character is: " + str(match_start))
-	print("Position of last character is: " + str(match_end))
-
+	#print("Position of first character is: " + str(match_start))
+	#print("Position of last character is: " + str(match_end))
+	print( "\t".join([result, str(match_start), str(match_end), str(len(result))]))
 
 
 
